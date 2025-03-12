f64cbc65b0286f2e877baa28fc5b7dc66c6468c1
KabsiMontassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-03-12 01:57:05 +0100

    push

--- src/app/frontoffice/frontoffice.module.ts
+++ src/app/frontoffice/frontoffice.module.ts
@@ -3,11 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { FrontofficeRoutingModule } from './frontoffice-routing.module';
 import { LayoutsComponent } from './layouts/layouts.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ErrorFrontComponent } from './components/error-front/error-front.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FarmingPracticeComponent } from './pages/farming-practice/farming-practice.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsdetailsComponent } from './pages/newsdetails/newsdetails.component';
+import { NewsComponent } from './pages/news/news.component';
+import { OurproductComponent } from './pages/ourproduct/ourproduct.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 
 @NgModule({
   declarations: [
     LayoutsComponent,
+    NavbarComponent,
+    FooterComponent,
+    ErrorFrontComponent,
+    AboutComponent,
+    ContactComponent,
+    FarmingPracticeComponent,
+    HomeComponent,
+    NewsdetailsComponent,
+    NewsComponent,
+    OurproductComponent,
+    ShopComponent,
 
   ],
   imports: [
--- src/app/frontoffice/layouts/layouts.component.ts
+++ src/app/frontoffice/layouts/layouts.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit , OnDestroy } from "@angular/core";
 
 @Component({
-  selector: 'app-layouts',
-  templateUrl: './layouts.component.html',
-  styleUrls: ['./layouts.component.css']
+  selector: "app-layouts",
+  templateUrl: "./layouts.component.html",
+  styleUrls: ["./layouts.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class LayoutsComponent {
+export class LayoutsComponent implements OnInit, OnDestroy {
+
+
+
+  ngOnInit() {
+
+    this.loadScripts([
+      "../../../assets/frontoffice/js/jquery.min.js",
+      "../../../assets/frontoffice/js/popper.min.js",
+      "../../../assets/frontoffice/js/bootstrap.min.js",
+
+      "../../../assets/frontoffice/js/owl.carousel.min.js",
+      "../../../assets/frontoffice/js/classynav.js",
+      "../../../assets/frontoffice/js/wow.min.js",
+
+      "../../../assets/frontoffice/js/jquery.sticky.js",
+      "../../../assets/frontoffice/js/jquery.magnific-popup.min.js",
+      "../../../assets/frontoffice/js/jquery.scrollup.min.js",
+
+      "../../../assets/frontoffice/js/jarallax.min.js",
+      "../../../assets/frontoffice/js/jarallax-video.min.js",
+      "../../../assets/frontoffice/js/active.js",
+
+    ]);
+
+
+  }
+  ngOnDestroy() {
+    this.removeScripts([
+      "../../../assets/frontoffice/js/jquery.min.js",
+      "../../../assets/frontoffice/js/popper.min.js",
+      "../../../assets/frontoffice/js/bootstrap.min.js",
+
+      "../../../assets/frontoffice/js/owl.carousel.min.js",
+      "../../../assets/frontoffice/js/classynav.js",
+      "../../../assets/frontoffice/js/wow.min.js",
+
+      "../../../assets/frontoffice/js/jquery.sticky.js",
+      "../../../assets/frontoffice/js/jquery.magnific-popup.min.js",
+      "../../../assets/frontoffice/js/jquery.scrollup.min.js",
+
+      "../../../assets/frontoffice/js/jarallax.min.js",
+      "../../../assets/frontoffice/js/jarallax-video.min.js",
+      "../../../assets/frontoffice/js/js/active.js",
+      
+    ]);
+  }
+
+
+
+
+
+  private loadScripts(scripts: string[]) {
+    scripts.forEach((script) => {
+      const scriptTag = document.createElement("script");
+      scriptTag.src = script;
+      scriptTag.async = true;
+      document.body.appendChild(scriptTag);
+    });
+  }
+
+
+
+  private removeScripts(scripts: string[]) {
+    scripts.forEach((script) => {
+      const scriptElements = document.body.getElementsByTagName("script");
+      for (let i = 0; i < scriptElements.length; i++) {
+        if (scriptElements[i].src === script) {
+          document.body.removeChild(scriptElements[i]);
+        }
+      }
+    });
+  }
 
 }
