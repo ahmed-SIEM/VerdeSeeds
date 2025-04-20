774cce5ae0cd7840e1d04fc9d6de40cac5fedb47
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-04-20 21:40:51 +0100

    update design

--- src/app/frontoffice/components/plateformeComps/others/sponsors/sponsors.component.ts
+++ src/app/frontoffice/components/plateformeComps/others/sponsors/sponsors.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+interface Sponsor{
+  datapartenariat: string;
+  logo: string;
+  nomSponsor: string;
+}
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
   styleUrls: ['./sponsors.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule],
 })
-export class SponsorsComponent {
-
+export class SponsorsComponent implements OnInit {
+  @Input() Sponsors: Sponsor[] = [];
+  @Input() color: string = 'blue';
+  
+  ngOnInit() {
+    // Set CSS variable based on sponsors count
+    document.documentElement.style.setProperty('--sponsor-count', this.Sponsors.length.toString());
+  }
 }
