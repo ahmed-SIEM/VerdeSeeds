dfe3714bca9ef458b900d4ad360e3c80281a5d0b
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-04-28 22:23:12 +0100

    push

--- src/app/services/plateforme/component.service.ts
+++ src/app/services/plateforme/component.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
@@ -11,34 +11,36 @@ export class componentServcie {
 
   constructor(private http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
   getComponents(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}`);
+    return this.http.get<any[]>(`${this.apiUrl}`, { headers: this.getHeaders() });
   }
 
   getComponent(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
   }
-
-
 
   createComponent(component: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}`, component);
+    return this.http.post<any>(`${this.apiUrl}`, component, { headers: this.getHeaders() });
   }
 
-  updateComponent( component: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}`, component);
+  updateComponent(component: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}`, component, { headers: this.getHeaders() });
   }
 
   deleteComponent(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
   }
 
   getAllcomponentsbyuserid(userId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/user/${userId}`);
+    return this.http.get<any[]>(`${this.apiUrl}/user/${userId}`, { headers: this.getHeaders() });
   }
 
   getusageRate(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/usageRate`)
+    return this.http.get(`${this.apiUrl}/usageRate`, { headers: this.getHeaders() });
   }
-
 }
